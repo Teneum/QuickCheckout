4a5a0407afc556a1bcb6184aa8353707ee6dbc7a
Shashank <2508.shashank@gmail.com>
2022-01-30 14:53:00 +0300

    Update

--- createadmindata.py
+++
@@ -1,7 +0,0 @@
-from DataFunc.accountmanagement import UserData
-userinfo = UserData()
-userid = '0001'
-userpassword = 'password'
-userinfo.delete_account(employeeID='0001')
-userinfo.createUser(userID= userid, password= userpassword, acccompl=False, username= 'Shashank Shukla',
-                    accountType='admin')
